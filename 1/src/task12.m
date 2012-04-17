61bf021478306e69b295013bad5a217616c85628
Erik Westrup <erik.westrup@gmail.com>
2012-04-17 22:34:49 +0200

    New interpretation of task 12.

--- 1/src/task12.m
+++ 1/src/task12.m
@@ -5,35 +5,38 @@ maxit = Inf;
 maxtime = 5*60*60; % In seconds. This is for each test i.e. the scripts total max time is 2*maxtime.
 
 % Find largest k for Gauss.
-n = 8;
+n_ge = 8;
 pr1.time_out(maxtime);
 can_solve = true;
 while can_solve
 	try
-		[orig_mean, pert_mean] = pr1.duosolve_gauss(n, solve_times);
+		[orig_mean, pert_mean] = pr1.duosolve_gauss(n_ge, solve_times);
 		percent = (pert_mean/orig_mean) * 100;
-		fprintf(1, 'Solved duo Gauss for k=%i, orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n', log2(n), orig_mean, pert_mean, percent);
-		n = bitshift(n, 1);
+		fprintf(1, 'Solved duo Gauss for k=%i, orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n', log2(n_ge), orig_mean, pert_mean, percent);
+		n_ge = bitshift(n_ge, 1);
 	catch ex
 		can_solve = false;
 		fprintf(2, 'Can''t solve more. Time out reached. \n');
 	end
 end
-fprintf(1, 'In %i seconds both the original and perturbed system could be solved with Gaussian elimination with a system 2^%i unknowns. For the largest system solved; orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n\n', maxtime, (log2(n)-1), orig_mean, pert_mean, percent);
+fprintf(1, 'In %i seconds both the original and perturbed system could be solved with Gaussian elimination with a system 2^%i unknowns. For the largest system solved; orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n\n', maxtime, (log2(n_ge)-1), orig_mean, pert_mean, percent);
 
 % Find largest k for SOR.
-n = 8;
+n_sor = 8;
 pr1.time_out(maxtime);
 can_solve = true;
 while can_solve
 	try
-		[orig_mean, pert_mean] = pr1.duosolve_sor(n, solve_times, maxit);
+		[orig_mean, pert_mean] = pr1.duosolve_sor(n_sor, solve_times, maxit);
 		percent = (pert_mean/orig_mean) * 100;
-		fprintf(1, 'Solved duo SOR for k=%i, orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n', log2(n), orig_mean, pert_mean, percent);
-		n = bitshift(n, 1);
+		fprintf(1, 'Solved duo SOR for k=%i, orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n', log2(n_sor), orig_mean, pert_mean, percent);
+		n_sor = bitshift(n_sor, 1);
 	catch ex
 		can_solve = false;
         fprintf(2, 'Can''t solve more. Time out reached. \n');
 	end
 end
-fprintf(1, 'In %i seconds both the original and perturbed system could be solved with SOR with a system 2^%i unknowns. For the largest system solved; orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n', maxtime, (log2(n)-1), orig_mean, pert_mean, percent);
+fprintf(1, 'In %i seconds both the original and perturbed system could be solved with SOR with a system 2^%i unknowns. For the largest system solved; orig_mean=%.5fs and pert_mean=%.5fs (i.e. %.2f%% of the original mean time).\n', maxtime, (log2(n_sor)-1), orig_mean, pert_mean, percent);
+
+k_best = log2(min(n_ge, n_sor)) - 1; % Subscract one since the n at termination is allready doubled.
+fprintf(1, '\nThe best n, the one that works for both GE and SOR, is n=2^%i\n', k_best);
--- 1/src/task13.m
+++ 1/src/task13.m
@@ -4,17 +4,16 @@ nbr_systems = 8;
 solve_times = 100;
 maxit = Inf;
 maxtime = Inf;
+n = 2^7; % From task 12.
 
 % GE
-n_ge = 2^7; % Fom task 12.
-means = pr1.multi_solve_gauss(nbr_systems, n_ge, solve_times, maxtime);
+means = pr1.multi_solve_gauss(nbr_systems, n, solve_times, maxtime);
 mean_ge = mean(means);
 means_str =  sprintf('%.10fs\n', means);
-fprintf(1, 'Solving  %i perturbed systems with GE with 2^%i unknowns %i times each gave the following mean solve times with a total mean of %.10fs:\n%s\n', nbr_systems, log2(n_ge), solve_times, mean_ge, means_str);
+fprintf(1, 'Solving  %i perturbed systems with GE with 2^%i unknowns %i times each gave the following mean solve times with a total mean of %.10fs:\n%s\n', nbr_systems, log2(n), solve_times, mean_ge, means_str);
 
 % SOR
-n_sor = 2^21; % Fom task 12.
-means = pr1.multi_solve_sor(nbr_systems, n_sor, solve_times, maxit, maxtime);
+means = pr1.multi_solve_sor(nbr_systems, n, solve_times, maxit, maxtime);
 mean_sor = mean(means);
 means_str =  sprintf('%.10fs\n', means);
-fprintf(1, 'Solving  %i perturbed systems with SOR with 2^%i unknowns %i times each gave the following mean solve times with a total mean of %.10fs:\n%s', nbr_systems, log2(n_sor), solve_times, mean_sor, means_str);
+fprintf(1, 'Solving  %i perturbed systems with SOR with 2^%i unknowns %i times each gave the following mean solve times with a total mean of %.10fs:\n%s', nbr_systems, log2(n), solve_times, mean_sor, means_str);
